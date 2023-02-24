4fa38a900afd13e91ecceeabd6655c0de14396d1
蒲龙山 <longshan.pu@kalendsoft.com>
2023-02-24 11:42:10 +0800

    去除template标签,保留空页面

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,7 +1,3 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue App"/>
-</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
