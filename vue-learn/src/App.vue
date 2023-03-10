6692f0aafcd3fe72df32d67a4de85e0700590e57
蒲龙山 <longshan.pu@kalendsoft.com>
2023-03-10 10:18:03 +0800

    插值表达式以及属性绑定基础训练

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,3 +1,7 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to vue3!"/>
+</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
--- vue-learn/src/components/HelloWorld.vue
+++ vue-learn/src/components/HelloWorld.vue
@@ -1,32 +1,51 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
-</template>
-
 <script>
 export default {
+  data() {
+    return {
+      toggle: true,
+      count: 1,
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      },
+
+    }
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.author.books.length > 0 ? 'yes' : 'no'
+    },
+
+
+  },
+  methods: {
+    num() {
+      this.count++;
+      // console.log(this.count);
+    },
+
+  },
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  
 }
 </script>
 
+<template>
+  <div class="hello">
+    <h1 v-if="toggle">{{ msg }}</h1>
+    <h2>{{ count }}</h2>
+    <button @click="num">ADD 1</button>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+  </div>
+</template>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
